10ba4af5ef085ed363a6fe0ec18742b02b859592
Choi Woochang <show4031132@gmail.com>
2024-01-31 18:16:41 +0900

    vue3 : form 형식의 component 구성

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <AppHeader></AppHeader>
-  <div>
-    {{message}}
-  </div>
-  <button @click="showAlert">경고</button>
+  <form action="">
+    <div>
+      <label for="userId">ID:</label>
+      <input id="userId" type="text" v-model="username">
+    </div>
 
+    <div>
+      <label for="password">PW:</label>
+      <input id="password" type="text" v-model="password">
+    </div>
+    <button type="submit">제출하기</button>
+  </form>
 </template>
 
 <script>
-  import AppHeader from './components/AppHeader.vue'
-
-export default {
-  components: {
-    // '컴포넌트 이름' : 컴포넌트 내용
-    //'AppHeader' : AppHeader
-    AppHeader
-  },  
-  data() {
-    return {
-      message: '앱 헤더 컴포넌트'
-    }
-  },
-  methods:{
-    showAlert(){
-      alert('hello')
-    }
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      }
+    },
   }
-}
 </script>
 
 <style scoped>
